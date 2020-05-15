201c8a71ec5453b31d3e532e340e9a0765db1c33
RomainPct <romain.penchenat@gmail.com>
2020-05-15 14:57:49 +0200

    Start my app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Header() {
+  const [search, setSearch] = React.useState('')
+  function submit(e) {
+    e.preventDefault()
+    console.log(search)
+    setSearch('')
+  }
+  return (
+    <header>
+      <h2>
+        <a href="/">Mon site</a>
+      </h2>
+      <p>
+        loremipsum {search}
+      </p>
+      <form onSubmitCapture={submit}>
+        <input
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          type="text"
+          />
+      </form>
+    </header>
+  )
+}
+
+function Row(props) {
+  return (
+    <div>
+      {props.text}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <Row text="yeahhhhhhh" />
+        <Row text="yeahhhhhhhh" />
+        <Row text="yeahhhhhhhhh" />
+      </main>
     </div>
   );
 }
