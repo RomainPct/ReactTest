7ae8b16da84125424283ec46f19f909cb9654b30
RomainPct <romain.penchenat@gmail.com>
2020-05-15 15:56:23 +0200

    Add reload button

--- src/App.js
+++ src/App.js
@@ -8,17 +8,23 @@ function App() {
         active_cryptocurrencies: 0,
         total_market_cap: []
     })
-    useEffect(() => {
+    function load() {
+        setCryptosData({
+            active_cryptocurrencies: 'loading...',
+            total_market_cap: []
+        })
         fetch('https://api.coingecko.com/api/v3/global')
             .then(response => response.json())
             .then(json => {
                 console.log(json.data)
                 setCryptosData(json.data)
             })
-    }, [])
+    }
+    useEffect(() => { load() }, [])
     return (
         <div className="app">
             <Header active_cryptocurrencies={cryptosData.active_cryptocurrencies} />
+            <button onClickCapture={load}>Reload</button>
             <main>
                 {Object.keys(cryptosData.total_market_cap).map(function(key, index) {
                     return <Row key={key} crypto={key} value={cryptosData.total_market_cap[key]} />
